828c87de98f79edfefc31fc1bab2717efe1fcb2d
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-10-21 00:13:05 +0530

    frontend completed

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 // styles
 import './RecipeList.css';
 function RecipeList({ recipes }) {
+  if (recipes.length === 0) {
+    return <div className="error">No Recipes to Load ...</div>;
+  }
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import RecipeList from '../../components/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
+
+// styles
+import './Search.css';
 
 function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get('q');
+  const url = 'http://localhost:3000/recipes?q=' + query;
+  const { error, isPending, data } = useFetch(url);
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Search;
