57f5f0b5311874fec1e3f8769a16107d5259b0ad
Gaurav Pawar <gauravpawar3102@gmail.com>
2022-10-20 22:58:54 +0530

    searchbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Searchbar from './SearchBar';
 
 function Navbar() {
   return (
@@ -8,6 +9,7 @@ function Navbar() {
         <Link to="/" className="brand">
           <h1>Cukng Nja</h1>
         </Link>
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
